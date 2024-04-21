a929528339c968bbbb0e757e54d984cfe25de9ce
ignacyszpilowski <ignacyszpilowski@gmail.com>
2024-04-21 23:19:35 +0200

    fetching user data as they logged in

--- js/choose_registration.js
+++ js/choose_registration.js
@@ -1,6 +1,24 @@
 const companyCreate = document.getElementById("company")
 const jobSeekerCreate = document.getElementById("job_seeker")
 const logoutButton = document.getElementById("logout")
+const userData = document.getElementById("use_data")
+
+const bearerToken = 'Bearer ' + localStorage.getItem('token')
+
+fetch('http://localhost:7070/SDEmpApp/main-page/user', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': bearerToken
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        userData.textContent = `Hello ${data.email}`
+    })
+    .catch(error => {
+        console.error('Error occurred during login:', error);
+    });
 
 logoutButton.onclick = function logout() {
     window.location.href = "../pages/main.html";
